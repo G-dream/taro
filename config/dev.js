4feabbb5e3c95f1c5dc2a2b9db6518e302a5cbc9
G-dream <50839105+G-dream@users.noreply.github.com>
2021-08-09 20:00:52 +0800

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,25 @@
+const isH5 = process.env.CLIENT_ENV === 'h5'
+const HOST = 'http://localhost:10088'
+
+module.exports = {
+  env: {
+    NODE_ENV: '"development"'
+  },
+  defineConstants: {
+    HOST:"/api"
+  },
+  mini: {},
+  h5: {
+    esnextModules: ['taro-ui'],
+    devServer: {
+      proxy: {
+        '/api/': {
+          target: HOST,
+          pathRewrite: {
+            '^/api/': '/'
+          },changeOrigin: true
+        }
+      }
+    }
+  }
+}
